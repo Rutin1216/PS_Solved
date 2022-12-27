4e98232a9e040edc8cba077fb02880778a2b9e63
Rutin1216 <105614079+Rutin1216@users.noreply.github.com>
2022-12-28 03:55:09 +0900

    Create P8958.cs
    8958번 문제 풀이

---
+++ c#/P8958.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace P8958
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int cnt = 0, add = 0;
+            StringBuilder builder = new StringBuilder();
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < num; i++)
+            {
+                builder.Append(Console.ReadLine());
+                foreach (var VARIABLE in builder.ToString())
+                {
+                    if (VARIABLE == 'O')
+                        add += ++cnt;
+                    else
+                        cnt = 0;
+                }
+                builder.Clear();
+                result.Append(add + "\n");
+                add = 0;
+                cnt = 0;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
