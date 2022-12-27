60abd5bcdb363eb5894745acba630f47213c0ad9
Rutin1216 <105614079+Rutin1216@users.noreply.github.com>
2022-12-28 02:50:40 +0900

    Create P2742.cs
    2742문제 풀이

---
+++ c#/P2742.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace P2742
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            StringBuilder builder = new StringBuilder();
+            for (int i = num; i > 0; i--)
+            {
+                builder.Append(i + "\n");
+            }
+            Console.WriteLine(builder);
+        }
+    }
+}
