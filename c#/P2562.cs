7b7a62666ef660a402d0582120d2380adae8133d
Rutin1216 <105614079+Rutin1216@users.noreply.github.com>
2022-12-27 19:54:13 +0900

    Create P2562.cs
    백준 2562번 문제

---
+++ c#/P2562.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P2562
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] num = new int[9];
+            int max = 0, index = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                num[i] = Convert.ToInt32(Console.ReadLine());
+                if (i == 0)
+                {
+                    max = num[i];
+                    index = i + 1;
+                }
+                else if(num[i] > max)
+                {
+                    max = num[i];
+                    index = i + 1;
+                }
+            }
+            Console.WriteLine(max);
+            Console.WriteLine(index);
+        }
+    }
+}
