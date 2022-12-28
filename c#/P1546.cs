e217526b5a3a7055c40294c15375e43df3813c60
Rutin1216 <105614079+Rutin1216@users.noreply.github.com>
2022-12-28 16:06:11 +0900

    1546번 문제 풀이

---
+++ c#/P1546.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P1546
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            string[] score = Console.ReadLine().Split(' ');
+            double[] myscore = new double[num];
+            for (int i = 0; i < num; i++)
+            {
+                myscore[i] = Convert.ToDouble(score[i]);
+            }
+
+            Array.Sort(myscore);
+            double max = myscore[num-1];
+            double avg = 0;
+            for (int i = 0; i < num; i++)
+            {
+                myscore[i] = myscore[i] / max * 100;
+                avg += myscore[i];
+            }
+            avg /= num;
+            Console.WriteLine(avg);
+        }
+    }
+}
