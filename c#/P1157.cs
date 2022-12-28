57529afc438f1f5cf5b580ec287037a5893111e1
Rutin1216 <105614079+Rutin1216@users.noreply.github.com>
2022-12-28 16:52:39 +0900

    12.28문제풀이

---
+++ c#/P1157.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P1577
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string word = Console.ReadLine().ToUpper();
+            Dictionary<char, int> result = new Dictionary<char, int>();
+            foreach (var ch in word)
+            {
+                if (result.Count == 0)
+                    result.Add(ch, 1);
+                else
+                {
+                    if (result.ContainsKey(ch))
+                        result[ch]++;
+                    else
+                        result.Add(ch, 1);
+                }
+            }
+            int maxvalue = 0;
+            char output = ' ';
+            foreach (var VARIABLE in result.Keys)
+            {
+                if (maxvalue < result[VARIABLE])
+                {
+                    maxvalue = result[VARIABLE];
+                    output = VARIABLE;
+                }
+                else if(maxvalue == result[VARIABLE])
+                {
+                    output = '?';
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
